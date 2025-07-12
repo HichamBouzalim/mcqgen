6d61a42cb4cd5de881f58ad7d999109d0c8981d5
Hicham Bouzalim <bouzalimhicham11@gmail.com>
2025-07-12 03:29:16 +0200

    updated

--- StreamlitAPP.py
+++ StreamlitAPP.py
@@ -3,8 +3,18 @@ import json
 import traceback
 import pandas as pd
 from dotenv import load_dotenv
-#from src.mcqgenerator.utils import read_file, get_table_data
+from src.mcqgenerator.utils import read_file, get_table_data
 import streamlit as st
 from langchain_community.callbacks import get_openai_callback
-#from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
+from src.mcqgenerator.MCQGenerator import generate_evaluate_chain
 from src.mcqgenerator.logger import logging
+
+
+
+#loading json file
+with open('C:\Users\Sniper\mcqgen\Response.json', 'r') as file:
+    RESPONSE_JSON = json.load(file)
+
+
+
+    #####  not finished   ####
